949106314c5e7ee068b8fa2ec3fa8a6d10250b85
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-03-13 15:52:37 +0000

    Added 4th Pokemon object to the pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 let pokemonList = [
   {name: 'Charmander', height: 2, types:['fire']},
   {name: 'Pikachu', height: 1.33, types:['electric']},
-  {name: 'Jigglypuff', height: 1.67, types:['normal', 'fairy']}
+  {name: 'Jigglypuff', height: 1.67, types:['normal', 'fairy']},
+  {name: 'Wigglytuff', height: 3.25, types:['normal', 'fairy']}
 ];
