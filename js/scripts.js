4c67aa2647243a7e1367554ecadb38ac35382621
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-03-13 15:49:10 +0000

    Added 3 Pokemon objects to the array; each with 3 keys.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-let pokemonList = [];
+let pokemonList = [
+  {name: 'Charmander', height: 2, types:['fire']},
+  {name: 'Pikachu', height: 1.33, types:['electric']},
+  {name: 'Jigglypuff', height: 1.67, types:['normal', 'fairy']}
+];
