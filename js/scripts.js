3ca74cc5ed171c66202cd9f1a11f9b932efb05fd
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-03-17 14:57:53 +0000

    Created a for loop to return the name and corresponding height of each Pokemon in the list
    I created this using "longhand" first, then converted to a template literal to practice this technique.

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,10 @@ let pokemonList = [
   {name: 'Jigglypuff', height: 1.67, types:['normal', 'fairy'], weaknesses:['steel', 'poison']},
   {name: 'Wigglytuff', height: 3.25, types:['normal', 'fairy'], weaknesses:['steel', 'poison']}
 ];
+
+// create a for loop that iterates over each item in pokemonList and returns its name and height
+for (let i = 0; i < pokemonList.length; i++) {
+//  document.write((pokemonList[i].name + ' ' + '(height: ' + pokemonList[i].height) + ')');
+//  using a template literal instead:
+  document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height})`);
+}
