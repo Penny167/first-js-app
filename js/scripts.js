3cbc0841b8811ba241f1d089106812d184d3f61a
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-03-23 17:19:17 +0000

    Copied code less comments as at end of task 4 to consolidate prior to working on task 5 changes (all prior work retained commented out above the new copy)
    No issues noted on the webpage after refreshing following this change.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-let pokemonList = [
+/*  let pokemonList = [
   {name: 'Charmander', height: 2, types:['fire'], weaknesses:['water', 'ground', 'rock']},
   {name: 'Pikachu', height: 1.33, types:['electric'], weaknesses:['ground']},
   {name: 'Jigglypuff', height: 1.67, types:['normal', 'fairy'], weaknesses:['steel', 'poison']},
@@ -14,13 +14,13 @@ document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height})`);
 
 /*  this time, I am storing my result as a variable so that I can neatly include
 it within html <p> tags. This will also display the results on separate lines  */
-for (let i = 0; i < pokemonList.length; i++) {
+/*  for (let i = 0; i < pokemonList.length; i++) {
   let pokemonStats = `${pokemonList[i].name} (height: ${pokemonList[i].height})`;
 //  document.write('<p>' + pokemonStats + '</p>');
 
 /*  now I am declaring a new variable in order to compare heights for the purpose
 of highlighting "special" pokemon */
-  let pokemonHeight = pokemonList[i].height;
+/*  let pokemonHeight = pokemonList[i].height;
 /*  now I am redefining the content that I want to display to include the result
 of the conditional statement within the <p> tags  */
 /*  if (pokemonHeight > 3) {
@@ -30,6 +30,26 @@ of the conditional statement within the <p> tags  */
 
 //  now I am rewriting the above code using a template literal to improve readability
 
+/*  if (pokemonHeight > 3) {
+    document.write(`<p>${pokemonStats} Wow,that's big!</p>`);
+  } else document.write(`<p>${pokemonStats}</p>`);
+}
+
+/*  At this stage, I am going to copy all of my current functioning code below
+without the comments to work on the next set of changes, whilst preserving
+the old version including the comments in a commented out version above  */
+
+
+let pokemonList = [
+  {name: 'Charmander', height: 2, types:['fire'], weaknesses:['water', 'ground', 'rock']},
+  {name: 'Pikachu', height: 1.33, types:['electric'], weaknesses:['ground']},
+  {name: 'Jigglypuff', height: 1.67, types:['normal', 'fairy'], weaknesses:['steel', 'poison']},
+  {name: 'Wigglytuff', height: 3.25, types:['normal', 'fairy'], weaknesses:['steel', 'poison']}
+];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  let pokemonStats = `${pokemonList[i].name} (height: ${pokemonList[i].height})`;
+  let pokemonHeight = pokemonList[i].height;
   if (pokemonHeight > 3) {
     document.write(`<p>${pokemonStats} Wow,that's big!</p>`);
   } else document.write(`<p>${pokemonStats}</p>`);
