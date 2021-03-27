a37da5768c69f03996985e9f8a5add4bccb26df3
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-03-27 14:36:17 +0000

    Appended the button(s) to the list item(s) as its child.

--- js/scripts.js
+++ js/scripts.js
@@ -96,4 +96,5 @@ pokemonRepository.getAll().forEach(function(pokemon) {
   let button = document.createElement('button');
   button.innerText = pokemon.name;
   button.classList.add('pokemon-button');
+  outputListItem.appendChild(button);
 });
