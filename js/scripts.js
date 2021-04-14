2172460376d9da6de7c5fdfff6926aeadf5e7058
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-04-14 19:16:38 +0100

    Added container for the modal header content, gave it the Bootstrap class "modal-header" and appended it to the modal content container.

--- js/scripts.js
+++ js/scripts.js
@@ -118,6 +118,9 @@ which has been created below to separate adding new items from adding new event 
     let modalContent = document.createElement('div');
     modalContent.classList.add('modal-content');
 
+    let modalHeader = document.createElement('div');
+    modalHeader.classList.add('modal-header');
+
     let closeButton = document.createElement('button');
     closeButton.classList.add('close-button');
     closeButton.innerText = 'x';
@@ -133,6 +136,7 @@ which has been created below to separate adding new items from adding new event 
     image.src = pokemon.imgUrl;
 
     modal.appendChild(modalContent);
+    modalContent.appendChild(modalHeader);
     modal.appendChild(closeButton);
     modal.appendChild(title);
     modal.appendChild(content);
